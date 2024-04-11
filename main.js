da1adac6d98fb6a2571c05b3b6205d356c907aa2
Angela Bressan <angelagreegorio@gmail.com>
2024-04-11 16:20:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio)
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+        }
+        else {
+            //alert('Elemento não encontrado');
+        conseole.log('Elemento não encontrado ou seletor inválido');
+        }
+    }
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//para//
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//template string//
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+    }
+}
+
+    tecla.onkeyup = function () {
+        tecla.classList.remover('ativa');
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
